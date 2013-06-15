ac8d0521d3c249f2d220da384c5317cf484c689c
Patz Huang <walkaniceday@gmail.com>
2013-06-15 13:38:43 +0800

    NAntFindTask

---
+++ NAntFindTasks/FindTask.cs
@@ -0,0 +1,20 @@
+﻿using NAnt.Core;
+using NAnt.Core.Attributes;
+
+namespace NAntFind
+{
+    [TaskName("find")]
+    public class FindTask : Task
+    {
+        [TaskAttribute("package", Required = true)]
+        public string PackageName { get; set; }
+
+        [TaskAttribute("required")]
+        public bool Required { get; set; }
+
+        protected override void ExecuteTask()
+        {
+            
+        }
+    }
+}
