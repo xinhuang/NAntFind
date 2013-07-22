c20de3879f6347ee88a61d151f977d19ea74d7f7
Patz Huang <walkaniceday@gmail.com>
2013-07-22 16:40:32 +0800

    Test: Find from registry.

--- TestNAntFind/IntegrationTest/NAntFindIntegrationTest.cs
+++ TestNAntFind/IntegrationTest/NAntFindIntegrationTest.cs
@@ -11,7 +11,13 @@ namespace TestNAntFind.IntegrationTest
         [TestMethod]
         public void a_big_test()
         {
-            Run(@"NAnt.build");
+            Run("NAnt.build");
+        }
+
+        [TestMethod]
+        public void should_find_visual_studio_from_registry()
+        {
+            Run("TestFindFromRegistry.build");
         }
 
         private static void Run(string script)
@@ -19,7 +25,7 @@ namespace TestNAntFind.IntegrationTest
             string nantPath = Path.Combine(Environment.CurrentDirectory, @"nant.exe");
             var process = new Process();
             process.StartInfo.FileName = nantPath;
-            process.StartInfo.Arguments = string.Format(@"-nologo -ext:NAntFindTasks.dll /f:{0}", script);
+            process.StartInfo.Arguments = string.Format(@"-nologo /f:{0} -v", script);
             process.StartInfo.UseShellExecute = false;
             process.StartInfo.RedirectStandardError = true;
             process.StartInfo.RedirectStandardInput = true;
