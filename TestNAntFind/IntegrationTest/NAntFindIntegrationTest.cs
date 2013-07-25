0a80cd29da2987f5b64334e118ac2b1f716805ce
Patz Huang <walkaniceday@gmail.com>
2013-07-25 14:59:04 +0800

    refactor

--- TestNAntFind/IntegrationTest/NAntFindIntegrationTest.cs
+++ TestNAntFind/IntegrationTest/NAntFindIntegrationTest.cs
@@ -8,53 +8,43 @@ namespace TestNAntFind.IntegrationTest
         [TestMethod]
         public void should_find_visual_studio_110_from_registry()
         {
-            Assert("TestFindFromRegistry.build");
+            NAnt.Assert("TestFindFromRegistry.build");
         }
 
         [TestMethod]
         public void should_find_visual_studio_110_from_hint()
         {
-            Assert("TestFindFromHint.build");
+            NAnt.Assert("TestFindFromHint.build");
         }
 
         [TestMethod]
         public void should_find_visual_studio_110_from_env()
         {
-            Assert("TestFindFromEnv.build");
+            NAnt.Assert("TestFindFromEnv.build");
         }
 
         [TestMethod]
         public void should_find_devenv_in_visual_studio_110()
         {
-            Assert("TestFindExistingFile.build");
+            NAnt.Assert("TestFindExistingFile.build");
         }
 
         [TestMethod]
         public void given_no_version_specified_should_return_default_version()
         {
-            Assert("GivenNoVersionShouldReturnDefault.build");
+            NAnt.Assert("GivenNoVersionShouldReturnDefault.build");
         }
 
         [TestMethod]
         public void given_no_version_specified_and_no_default_should_fail()
         {
-            AssertFail("GivenNoVersionSpecifiedAndDefaultMissingShouldFail.build");
+            NAnt.AssertFail("GivenNoVersionSpecifiedAndDefaultMissingShouldFail.build");
         }
 
         [TestMethod]
         public void given_no_version_specified_in_find_and_module_should_return_only_one_as_implicit_default()
         {
-            Assert("TestFindImplicitDefault.build");
-        }
-
-        private static void AssertFail(string script)
-        {
-            NAnt.AssertFail(script);
-        }
-
-        private static void Assert(string script)
-        {
-            NAnt.Assert(script);
+            NAnt.Assert("TestFindImplicitDefault.build");
         }
     }
 }
